e712d5690addcd7842285df335f8270763669d86
lhiro <1074863419@qq.com>
2025-05-05 16:12:08 +0800

    try vercel deploy

--- next.config.js
+++ next.config.js
@@ -17,6 +17,14 @@ const nextConfig = {
   typescript: {
     // ignoreBuildErrors: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig 
